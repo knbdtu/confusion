9d1b3bbba14e4d48ae0ac1440196bb9bfa8e3d43
Nitin Bhardwaj Komaravolu <knbdtu@hotmail.com>
2018-08-31 00:56:43 +0530

    [Week 1] Exercise 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			dishes: DISHES
+		};
+	}
 	render() {
 		return (
 			<div className="App">
